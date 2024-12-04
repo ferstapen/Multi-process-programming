bb61f429569e5bc039b697dde2e1c50465f1da31
ferstapen <164868955+ferstapen@users.noreply.github.com>
2024-12-04 11:16:11 +0500

    Update module_10_5.py

--- module_10_5.py
+++ module_10_5.py
@@ -12,19 +12,20 @@ def read_info(name):
 
 filenames = ['file 1.txt', 'file 2.txt', 'file 3.txt', 'file 4.txt']
 
-# Линейный подход
-start_time = time.time()
-for i in filenames:
-    read_info(i)
-end_time = time.time()
-result_time = end_time - start_time
-print(f'Линейное время: {result_time}')
 
 if __name__ == '__main__':
+    # Линейный подход
+    start_time = time.time()
+    for i in filenames:
+        read_info(i)
+    end_time = time.time()
+    result_time = end_time - start_time
+    print(f'Линейное время: {result_time}')
+
     # Многопроцессный подход
     start_time_2 = time.time()
-    with multiprocessing.Pool() as pool:
+    with multiprocessing.Pool(processes=4) as pool:
         pool.map(read_info, filenames)
     end_time_2 = time.time()
     result_time_2 = end_time - start_time
-    print(f'многопроцессное время: {result_time_2}')
+    print(f'Многопроцессное время: {result_time_2}')
